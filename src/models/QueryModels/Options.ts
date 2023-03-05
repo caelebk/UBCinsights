54016163e85e631606c87172bb331a6d1e49f8bc
Caeleb <khocaeleb@gmail.com>
2023-03-04 20:28:38 -0800

    updated and added query models

--- src/models/QueryModels/Options.ts
+++ src/models/QueryModels/Options.ts
@@ -1,21 +1,39 @@
-import {Key} from "./Keys";
+import {AnyKey} from "./Keys";
+import {Direction} from "./Enums";
 
 export default class Options {
-	private readonly _columns: Key[];
-	private readonly _order: Key | undefined;
+	private readonly _columns: AnyKey[];
+	private readonly _sort: Sort | undefined;
 
-	constructor(columnsQuery: Key[], orderQuery?: Key) {
+	constructor(columnsQuery: AnyKey[], sort?: Sort) {
 		this._columns = columnsQuery;
-		if (orderQuery) {
-			this._order = orderQuery;
+		if (sort) {
+			this._sort = sort;
 		}
 	}
 
-	public get columns(): Key[] {
+	public get columns(): AnyKey[] {
 		return this._columns;
 	}
 
-	public get order(): Key | undefined {
+	public get sort(): Sort | undefined {
+		return this._sort;
+	}
+}
+
+interface Order {
+	dir: Direction,
+	keys: AnyKey[]
+}
+
+export class Sort {
+	private readonly _order: AnyKey | Order;
+
+	constructor(order: AnyKey | Order) {
+		this._order = order;
+	}
+
+	public get order(): AnyKey | Order {
 		return this._order;
 	}
 }
