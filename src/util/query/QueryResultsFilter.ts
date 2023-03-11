5ae69bc69c348d88078475ce01d754d21cbc9e3d
Caeleb <khocaeleb@gmail.com>
2023-03-10 22:18:09 -0800

    refactor & implemented aggregation

--- src/util/query/QueryResultsFilter.ts
+++ src/util/query/QueryResultsFilter.ts
@@ -1,24 +1,74 @@
-import {InsightError, InsightResult} from "../../controller/IInsightFacade";
+import {InsightError, InsightResult, ResultTooLargeError} from "../../controller/IInsightFacade";
 import Options, {Order} from "../../models/QueryModels/Options";
 import {Section} from "../../models/DatasetModels/Section";
-import {AnyKey, Key, MKey, SKey} from "../../models/QueryModels/Keys";
+import {AnyKey, ApplyKey, Key, MKey, SKey} from "../../models/QueryModels/Keys";
 import Transformations, {ApplyRule} from "../../models/QueryModels/Transformations";
-import {SField} from "../../models/QueryModels/Enums";
+import aggregateSections from "./QueryAggregate";
 
 export default function filterResults(options: Options,
 									  sections: Section[],
 									  datasetId: string,
 									  transformations?: Transformations): InsightResult[] {
-	let groups: Map<string, Section[]>;
-	let transformedData: Map<string, number>;
-	if (transformations) {
-		groups = groupData(transformations.group, sections, new Map<string, Section[]>());
-		transformData(transformations.applyRules, groups);
-	}
+
 	if (options.order) {
 		sections = sortResults(options.order, sections);
 	}
-	const columnKeys: AnyKey[] = options.columns;
+	let results: InsightResult[];
+	if (transformations) {
+		results = transformationResults(transformations, options.columns, sections, datasetId);
+		if (results.length > 5000) {
+			throw new ResultTooLargeError("There were more than 5000 results with this query.");
+		} else {
+			return results;
+		}
+	} else {
+		results = vanillaResults(options.columns, sections, datasetId);
+		if (results.length > 5000) {
+			throw new ResultTooLargeError("There were more than 5000 results with this query.");
+		} else {
+			return results;
+		}
+	}
+}
+
+export function transformationResults(transformations: Transformations,
+									  columns: AnyKey[],
+									  sections: Section[],
+									  datasetId: string): InsightResult[] {
+	let groups: Map<string, Section[]>;
+	let insightResults: InsightResult[] = [];
+	groups = groupData(transformations.group, sections, new Map<string, Section[]>());
+	groups.forEach((grouped_sections: Section[], grouped_key: string) => {
+		let insightResult: InsightResult = {};
+		columns.forEach((columnKey: AnyKey) => {
+			if (!(columnKey instanceof ApplyKey)) {
+				if (grouped_sections.length > 0) {
+					filterKeys(columnKey, grouped_sections[0], insightResult, datasetId);
+				} else {
+					throw new InsightError("missing section");
+				}
+			}
+		});
+		let appliedRules: Map<string, number> = transformData(transformations.applyRules, grouped_sections);
+		appliedRules.forEach((aggregated_value: number, applyKey: string) => {
+			insightResult[applyKey] = aggregated_value;
+		});
+		insightResults.push(insightResult);
+	});
+	return insightResults;
+}
+
+function transformData(rules: ApplyRule[], sections: Section[]): Map<string, number> {
+	let map: Map<string, number> = new Map<string, number>();
+	rules.forEach((rule: ApplyRule) => {
+		const id: string = rule.id;
+		const value: number = aggregateSections(rule.key, rule.applyToken, sections);
+		map.set(id, value);
+	});
+	return map;
+}
+
+export function vanillaResults(columnKeys: AnyKey[], sections: Section[], datasetId: string): InsightResult[] {
 	return sections.map((section: Section) => {
 		let insightResult: InsightResult = {};
 		columnKeys.forEach((key: AnyKey) => {
@@ -55,62 +105,13 @@ function updateGroup(groups: Map<string, Section[]>, value: string, section: Sec
 	}
 }
 
-// Transform grouped sections to aggregated values.
-function transformData(rules: ApplyRule[], groups: Map<string, Section[]>): Map<string, number> {
-	return new Map<string, number>();
-}
-
-function findMax(sections: Section[], key: Key): number {
-	if (key instanceof MKey) {
-		return sections.reduce((prev: Section, current: Section) => {
-			return (prev.getMFieldValue(key.mField) > current.getMFieldValue(key.mField)) ? prev : current;
-		}).getMFieldValue(key.mField);
-	} else {
-		throw new InsightError("Cannot aggregate MAX for SKeys");
-	}
-}
-
-function findAvg(sections: Section[], key: Key): number {
-	return findSum(sections, key) / sections.length;
-}
-
-function findMin(sections: Section[], key: Key): number {
-	if (key instanceof MKey) {
-		return sections.reduce((prev: Section, current: Section) => {
-			return (prev.getMFieldValue(key.mField) < current.getMFieldValue(key.mField)) ? prev : current;
-		}).getMFieldValue(key.mField);
-	} else {
-		throw new InsightError("Cannot aggregate MIN for SKeys");
-	}
-}
-
-function findCount(sections: Section[], key: Key): number {
-	if (key instanceof MKey) {
-		let mFieldValues: number[] = sections.map((section: Section) => section.getMFieldValue(key.mField));
-		return new Set<number>(mFieldValues).size;
-	} else {
-		let sFieldValues: string[] = sections.map((section: Section) => section.getSFieldValue(key.sField));
-		return new Set<string>(sFieldValues).size;
-	}
-}
-
-function findSum(sections: Section[], key: Key): number {
-	if (key instanceof MKey) {
-		return sections.reduce((sum: number, current: Section) => {
-			return sum + current.getMFieldValue(key.mField);
-		}, 0);
-	} else {
-		throw new InsightError("Cannot aggregate MIN for SKeys");
-	}
-}
-
 function filterKeys(key: AnyKey, section: Section, insightResult: InsightResult, datasetId: string): void {
 	if (key instanceof MKey) {
 		insightResult[datasetId.concat("_", key.mField)] = section.getMFieldValue(key.mField);
 	} else if (key instanceof SKey) {
 		insightResult[datasetId.concat("_", key.sField)] = section.getSFieldValue(key.sField);
 	} else {
-		// TODO: Implement Apply Key filter.
+		throw new InsightError("ApplyKey shouldn't have been passed in");
 	}
 }
 
@@ -119,6 +120,7 @@ function sortResults(key: Order, sections: Section[]): Section[] {
 		return sortingPrecedence(key, section1, section2);
 	});
 }
+
 function sortingPrecedence(key: Order, section1: Section, section2: Section): number {
 	if (key instanceof MKey) {
 		return section1.getMFieldValue(key.mField) > section2.getMFieldValue(key.mField) ? 1 : -1;
@@ -130,24 +132,3 @@ function sortingPrecedence(key: Order, section1: Section, section2: Section): nu
 }
 
 
-let sections: Section[] = [
-	new Section({id: "1", Course: "cpsc", Subject: "sci",
-		Professor: "Jean",  Avg: 90, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "2", Course: "cpsc", Subject: "sci",
-		Professor: "Jean",  Avg: 80, Section: "1900", Title : "310", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "3", Course: "cpsc", Subject: "sci",
-		Professor: "Casey",  Avg: 95, Section: "1900", Title : "310", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "4", Course: "cpsc", Subject: "sci",
-		Professor: "Casey",  Avg: 85, Section: "1900", Title : "310", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "5", Course: "cpsc", Subject: "sci",
-		Professor: "Kelly",  Avg: 74, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "6", Course: "cpsc", Subject: "sci",
-		Professor: "Kelly",  Avg: 78, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "7", Course: "cpsc", Subject: "sci",
-		Professor: "Kelly",  Avg: 72, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
-	new Section({id: "8", Course: "cpsc", Subject: "sci",
-		Professor: "Eli",  Avg: 85, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
-];
-
-console.log(groupData([new SKey(SField.instructor), new SKey(SField.title)],
-	sections, new Map<string, Section[]>()));
