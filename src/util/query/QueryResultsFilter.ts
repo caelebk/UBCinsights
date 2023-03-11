d865be71fa8e31620e0722e8ea3c40990f9bcfa2
caeleb <khocaeleb@gmail.com>
2023-03-10 16:17:21 -0800

    temp commit

--- src/util/query/QueryResultsFilter.ts
+++ src/util/query/QueryResultsFilter.ts
@@ -3,15 +3,17 @@ import Options, {Order} from "../../models/QueryModels/Options";
 import {Section} from "../../models/DatasetModels/Section";
 import {AnyKey, Key, MKey, SKey} from "../../models/QueryModels/Keys";
 import Transformations, {ApplyRule} from "../../models/QueryModels/Transformations";
+import {SField} from "../../models/QueryModels/Enums";
 
 export default function filterResults(options: Options,
 									  sections: Section[],
 									  datasetId: string,
 									  transformations?: Transformations): InsightResult[] {
-	let group: Map<string, Section[]> = new Map<string, Section[]>();
+	let groups: Map<string, Section[]>;
+	let transformedData: Map<string, number>;
 	if (transformations) {
-		groupData(transformations.group, sections, group);
-		transformData(transformations.applyRules, sections);
+		groups = groupData(transformations.group, sections, new Map<string, Section[]>());
+		transformData(transformations.applyRules, groups);
 	}
 	if (options.order) {
 		sections = sortResults(options.order, sections);
@@ -27,18 +29,20 @@ export default function filterResults(options: Options,
 }
 
 function groupData(groupKeys: Key[], sections: Section[], map: Map<string, Section[]>): Map<string, Section[]> {
-	groupKeys.forEach((key: Key) => {
-		sections.reduce((groups: Map<string, Section[]>, current: Section) => {
+	sections.reduce((groups: Map<string, Section[]>, current: Section) => {
+		let value: string = "";
+		groupKeys.forEach((key: Key) => {
 			if (key instanceof MKey) {
-				let value: string = String(current.getMFieldValue(key.mField));
-				updateGroup(groups, value, current);
+				value += String(current.getMFieldValue(key.mField));
 			} else {
-				let value: string = current.getSFieldValue(key.sField);
-				updateGroup(groups, value, current);
+				value += current.getSFieldValue(key.sField);
 			}
-			return groups;
-		}, map);
-	});
+			value += "_";
+		});
+		value = value.substring(0, value.length - 1);
+		updateGroup(groups, value, current);
+		return groups;
+	}, map);
 	return map;
 }
 
@@ -51,8 +55,9 @@ function updateGroup(groups: Map<string, Section[]>, value: string, section: Sec
 	}
 }
 
-function transformData(rules: ApplyRule[], sections: Section[]): Section[] {
-	return [];
+// Transform grouped sections to aggregated values.
+function transformData(rules: ApplyRule[], groups: Map<string, Section[]>): Map<string, number> {
+	return new Map<string, number>();
 }
 
 function findMax(sections: Section[], key: Key): number {
@@ -104,6 +109,8 @@ function filterKeys(key: AnyKey, section: Section, insightResult: InsightResult,
 		insightResult[datasetId.concat("_", key.mField)] = section.getMFieldValue(key.mField);
 	} else if (key instanceof SKey) {
 		insightResult[datasetId.concat("_", key.sField)] = section.getSFieldValue(key.sField);
+	} else {
+		// TODO: Implement Apply Key filter.
 	}
 }
 
@@ -118,6 +125,29 @@ function sortingPrecedence(key: Order, section1: Section, section2: Section): nu
 	} else if (key instanceof SKey) {
 		return section1.getSFieldValue(key.sField).localeCompare(section2.getSFieldValue(key.sField));
 	}
-	// TODO: handle rooms which hasn't been implemented yet
+	// TODO: handle ApplyKey
 	return 0;
 }
+
+
+let sections: Section[] = [
+	new Section({id: "1", Course: "cpsc", Subject: "sci",
+		Professor: "Jean",  Avg: 90, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "2", Course: "cpsc", Subject: "sci",
+		Professor: "Jean",  Avg: 80, Section: "1900", Title : "310", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "3", Course: "cpsc", Subject: "sci",
+		Professor: "Casey",  Avg: 95, Section: "1900", Title : "310", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "4", Course: "cpsc", Subject: "sci",
+		Professor: "Casey",  Avg: 85, Section: "1900", Title : "310", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "5", Course: "cpsc", Subject: "sci",
+		Professor: "Kelly",  Avg: 74, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "6", Course: "cpsc", Subject: "sci",
+		Professor: "Kelly",  Avg: 78, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "7", Course: "cpsc", Subject: "sci",
+		Professor: "Kelly",  Avg: 72, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
+	new Section({id: "8", Course: "cpsc", Subject: "sci",
+		Professor: "Eli",  Avg: 85, Section: "1900", Title : "210", Year:100, Audit:10, Fail:20, Pass: 10}),
+];
+
+console.log(groupData([new SKey(SField.instructor), new SKey(SField.title)],
+	sections, new Map<string, Section[]>()));
