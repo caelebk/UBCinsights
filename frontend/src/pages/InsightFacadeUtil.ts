22ceee390fab2fe206a11c23105d643ca4ead679
caeleb <khocaeleb@gmail.com>
2023-03-27 20:34:46 -0700

    Finished get/post requests

--- frontend/src/pages/InsightFacadeUtil.ts
+++ frontend/src/pages/InsightFacadeUtil.ts
@@ -9,21 +9,21 @@ interface Dataset {
 	numRows: number
 }
 
-interface Result {
+export interface InsightResult {
 	[key: string]: string | number;
 }
 
 export async function getDatasets(state: string): Promise<string[]> {
 	const response = await fetch(
-		"/datasets",
+		"http://localhost:4321/datasets",
 		{
 			method:"GET",
-			headers:{}
+			headers:{"Content-Type": "application/json"}
 		}
 	)
 	let validDatasetIDs: string[];
 	const data = await response.json();
-	const datasets: Dataset[] = data as Dataset[];
+	const datasets: Dataset[] = data.result as Dataset[];
 	if (state === "Section") {
 		const sectionDatasets = datasets.filter((dataset: Dataset) => {
 			return dataset.kind === DatasetKind.Sections;
@@ -42,16 +42,18 @@ export async function getDatasets(state: string): Promise<string[]> {
 	return Promise.resolve(validDatasetIDs);
 }
 
-export async function sendQuery(query: string): Promise<string[][]> {
+export async function sendQuery(query: string): Promise<InsightResult[]> {
 	const response = await fetch(
-		"/query",
+		"http://localhost:4321/query",
 		{
 			method:"POST",
-			headers:{},
+			headers:{"Content-Type": "application/json"},
 			body: query
 		}
 	)
 	const data = await response.json();
-	const results = data as Result[];
-	return [];
+	if (!response.ok || response.status === 400) {
+		return Promise.reject("Status code " + response.status + ":\n" + data.error);
+	}
+	return Promise.resolve(data.result as InsightResult[]);
 }
